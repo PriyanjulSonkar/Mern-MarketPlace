a7a07bc8e300afdd4e011c779027fdf6fc37c923
PriyanjulSonkar <priyanjulsonkar@gmail.com>
2026-06-28 14:01:51 +0530

    feat: implement frontend components for products, orders, auctions, and cart management with corresponding server controller

--- server/controllers/order.controller.js
+++ server/controllers/order.controller.js
@@ -1,4 +1,4 @@
-import {Order, CartItem} from '../models/order.model'
+import { Order, CartItem } from '../models/order.model'
 import errorHandler from './../helpers/dbErrorHandler'
 import config from '../../config/config'
 import Razorpay from 'razorpay'
@@ -10,7 +10,7 @@ const create = async (req, res) => {
     let order = new Order(req.body.order)
     let result = await order.save()
     res.status(200).json(result)
-  } catch (err){
+  } catch (err) {
     return res.status(400).json({
       error: errorHandler.getErrorMessage(err)
     })
@@ -19,12 +19,12 @@ const create = async (req, res) => {
 
 const listByShop = async (req, res) => {
   try {
-    let orders = await Order.find({"products.shop": req.shop._id})
-      .populate({path: 'products.product', select: '_id name price'})
+    let orders = await Order.find({ "products.shop": req.shop._id })
+      .populate({ path: 'products.product', select: '_id name price' })
       .sort('-created')
       .exec()
     res.json(orders)
-  } catch (err){
+  } catch (err) {
     return res.status(400).json({
       error: errorHandler.getErrorMessage(err)
     })
@@ -33,11 +33,11 @@ const listByShop = async (req, res) => {
 
 const update = async (req, res) => {
   try {
-    let order = await Order.updateOne({'products._id':req.body.cartItemId}, {
-        'products.$.status': req.body.status
+    let order = await Order.updateOne({ 'products._id': req.body.cartItemId }, {
+      'products.$.status': req.body.status
     })
-      res.json(order)
-  } catch (err){
+    res.json(order)
+  } catch (err) {
     return res.status(400).json({
       error: errorHandler.getErrorMessage(err)
     })
@@ -57,7 +57,7 @@ const orderByID = async (req, res, next, id) => {
       })
     req.order = order
     next()
-  } catch (err){
+  } catch (err) {
     return res.status(400).json({
       error: errorHandler.getErrorMessage(err)
     })
@@ -65,12 +65,12 @@ const orderByID = async (req, res, next, id) => {
 }
 
 const listByUser = async (req, res) => {
-  try{
+  try {
     let orders = await Order.find({ "user": req.profile._id })
-        .sort('-created')
-        .exec()
+      .sort('-created')
+      .exec()
     res.json(orders)
-  } catch (err){
+  } catch (err) {
     return res.status(400).json({
       error: errorHandler.getErrorMessage(err)
     })
@@ -91,15 +91,15 @@ const createRazorpayOrder = async (req, res) => {
       key_id: config.razorpay_key_id,
       key_secret: config.razorpay_key_secret
     })
-    
+
     const amount = Math.round(req.body.amount * 100)
-    
+
     const options = {
       amount: amount,
-      currency: "USD",
+      currency: "INR",
       receipt: "receipt_order_" + Date.now()
     }
-    
+
     const order = await razorpay.orders.create(options)
     res.status(200).json(order)
   } catch (err) {
